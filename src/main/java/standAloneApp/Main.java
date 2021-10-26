a8c18746402dba5e99270b49c772c6ee97e19930
sohamsavalapurkar <soham1999@gmail.com>
2021-10-25 23:10:55 -0500

    Null pointer solved

--- src/main/java/standAloneApp/Main.java
+++ src/main/java/standAloneApp/Main.java
@@ -4,15 +4,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import standAloneApp.frontend.GUI;
+
+import java.awt.*;
 
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.setHeadless(false);
-        app.run(args);
+        //SpringApplication app = new SpringApplication(Main.class);
+        //app.setBannerMode(Banner.Mode.OFF);
+        //app.setHeadless(false);
+        //app.run(args);
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Main.class).headless(false).run();
+        EventQueue.invokeLater(() ->{
+            GUI ex = ctx.getBean(GUI.class);
+            ex.setVisible(true);
+
+        });
     }
 
     @Override
--- src/main/java/standAloneApp/frontend/GUI.java
+++ src/main/java/standAloneApp/frontend/GUI.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class GUI{
+public class GUI implements ActionListener{
     JFrame f = new JFrame();
     JFrame librarian, searchResults;
     JButton login, loginSelect, searchButton, issueBook, returnBook, addBook, addMultipleBook, addUser, addMultipleUser, showUserDetails, issuedBook, returnedBook, addedBook, addedUser, logout;
@@ -26,9 +26,22 @@ public class GUI{
     @Autowired
     ContactService contactService;
 
+    public void setVisible(boolean b) {
+        f.setVisible(b);
+        try{
+            f.setSize(1280,720);
+            f.setResizable(true);
+            f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mainPage();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    private void mainPage() {
 
-    private void mainPage(){
         f.setLayout(null);
 
         JLabel title = new JLabel("Library Management");
@@ -56,6 +69,7 @@ public class GUI{
         loginSelect.setLocation(x+850, 100);
 
         List<Contact> temp = new ArrayList<>();
+
         temp = contactService.getContacts();
         for(int i=0; i<15; i++) {
             System.out.println(temp.get(i).getFname());
@@ -69,19 +83,57 @@ public class GUI{
         f.setVisible(true);
     }
 
+    private void loginPage() {
+        f.setLayout(null);
+        JLabel title = new JLabel("Librarian Login");
+        title.setSize(1000, 200);
+        title.setFont(new Font(title.getFont().getName(), Font.BOLD, Math.min(30, title.getHeight())));
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setLocation((f.getWidth()-title.getWidth())/2, 50);
+        JLabel loginLabel = new JLabel("Login ID", SwingConstants.CENTER);
+        loginLabel.setSize(100, 30);
+        JLabel passwordLabel = new JLabel("Password", SwingConstants.CENTER);
+        passwordLabel.setSize(100, 30);
+        loginField = new JTextField();
+        loginField.setSize(300, 30);
+        int x = (f.getWidth()-loginField.getWidth())/2;
+        loginField.setLocation(x, 300);
+        loginLabel.setLocation((x-100), 300);
+        passwordField = new JPasswordField();
+        passwordField.setSize(300, 30);
+        x = (f.getWidth()-passwordField.getWidth())/2;
+        passwordField.setLocation(x,400);
+        passwordLabel.setLocation((x-100), 400);
+        login = new JButton("Login");
+        login.setSize(250,30);
+        login.setLocation((f.getWidth()-login.getWidth())/2, 500);
+        login.addActionListener(this);
+        f.add(title);
+        f.add(loginLabel);
+        f.add(loginField);
+        f.add(passwordLabel);
+        f.add(passwordField);
+        f.add(login);
+        f.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == loginSelect) {
+            f.getContentPane().removeAll();
+            f.revalidate();
+            f.repaint();
+            loginPage();
+
+
+        }
+        if(e.getSource() == searchButton) {
+
+        }
+    }
 
 
 
     public GUI(){
-        try{
-            f.setSize(1280,720);
-            f.setResizable(true);
-            f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            mainPage();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+
     }
 }
